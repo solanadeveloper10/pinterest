8c059e01c55540902b43f631bb887b3dc908a5f8
solanadeveloper10 <solanadeveloper10@gmail.com>
2025-01-23 16:01:07 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='wrapper'>
+      <aside className='toolbar'>
+        <div className='socials-container'>
+          <a className="social-link" href="/">
+            <img className="social-img" src="/images/icons/pinterest.png" />
+          </a>
+          <a className="social-link" href="#twitter">
+            <img className="social-img" src="/images/icons/x.jpg" />
+          </a>
+          <a className="social-link" href="#telegram">
+            <img className="social-img" src="/images/icons/tg.png" />
+          </a>
+          <a className="social-link" href="#dexscreener">
+            <img className="social-img" src="/images/icons/dex_screener.jpg" />
+          </a>
+          <a className="social-link" href="#dextools">
+            <img className="social-img" src="/images/icons/dextools.jpg" />
+          </a>
+        </div>
+      </aside>
+      <section className='content'>
+        <h1 className='title'>Pinterest</h1>
+
+        <p className='contract'>
+          CA:&nbsp;trumpdJmqbcytBWLYbtY8Mf6BR8azw4Ht3sZbsY25v3
+        </p>
+      </section>
+    </div>
+  )
+}
+
+export default App
